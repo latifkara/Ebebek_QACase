c45e575e9c63cc831aaa2f2386865d1087179498
latifkara <latifkara93@gmail.com>
2022-11-25 10:50:51 +0300

    Main1 | Add QA case

---
+++ src/main/java/Framework/ConfigReader.java
@@ -0,0 +1,12 @@
+package Framework;
+
+import java.util.Properties;
+
+public class ConfigReader {
+
+    private Properties properties;
+
+    public ConfigReader(){
+        properties = new Properties();
+    }
+}

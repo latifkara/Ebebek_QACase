c1542aa925c936fa7892610505c94022679b225e
latifkara <latifkara93@gmail.com>
2022-12-04 10:17:38 +0300

    Main 6 | Test Mobile Page.

--- src/test/java/Devices/mappingAppium.java
+++ src/test/java/Devices/mappingAppium.java
@@ -14,12 +14,17 @@ public class mappingAppium {
 
         DesiredCapabilities desired = new DesiredCapabilities();
         desired.setCapability(CapabilityType.BROWSER_NAME, "");
-        desired.setCapability("appium:deviceName", "Galaxy S4");
+        desired.setCapability("appium:deviceName", "Pixel 4 API 30");
         desired.setCapability("appium:automationName", "uiautomator2");
-        desired.setCapability("appium:platformVersion", "4.4");
+        desired.setCapability("appium:platformVersion", "11.0");
         desired.setCapability("appium:platformName", "Android");
         desired.setCapability("appium:skipUnlock", "true");
-        desired.setCapability("appium:app", System.getProperty("user.dir") + "\\src\\test\\resource\\App\\ebebek.apk");
+//        desired.setCapability("appium:fullReset", "true");
+//        desired.setCapability("appium:noReset", "false");
+        desired.setCapability("appium:newCommandTimeout", 3600);
+        desired.setCapability("appium:connectHardwareKeyboard", true);
+
+       // desired.setCapability("appium:app", System.getProperty("user.dir") + "\\src\\test\\resource\\App\\ebebek.apk");
 
         driver = new AndroidDriver(new URL("http://127.0.0.1:4723/"), desired);
         return driver;
--- src/test/java/MobilePages/mobileAppTesting.java
+++ src/test/java/MobilePages/mobileAppTesting.java
@@ -3,17 +3,27 @@ package MobilePages;
 
 import Devices.mappingAppium;
 import io.appium.java_client.AppiumDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.testng.annotations.Test;
 
 import java.net.MalformedURLException;
+import java.security.Key;
 
 public class mobileAppTesting {
+    By search = By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[1]/android.widget.FrameLayout[2]/androidx.recyclerview.widget.RecyclerView/android.widget.FrameLayout[1]/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.EditText");
     public AppiumDriver driver;
     mappingAppium appium = new mappingAppium();
 
     @Test
-    public void mobileAppTest() throws MalformedURLException {
+    public void mobileAppTest() throws MalformedURLException, InterruptedException {
         driver = appium.setMobile();
-        driver.get("https://www.google.com");
+        driver.findElement(By.xpath("//android.widget.TextView[@content-desc='Chrome']")).click();
+        Thread.sleep(4000);
+        driver.findElement(search).click();
+        driver.findElement(search).clear();
+        driver.findElement(search).sendKeys("https://ebebek.com");
+        driver.findElement(search).sendKeys(Keys.ENTER);
+
     }
 }
